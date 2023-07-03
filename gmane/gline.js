72a9f5201743a194f09a260b463ab9132a2ec0ce
Awais Saeed <awaissaeed530@gmail.com>
2023-07-03 16:37:11 +0500

    gmane

---
+++ gmane/gline.js
@@ -0,0 +1,3 @@
+gline = [ ['Month','umich.edu','virginia.edu','unicon.net','cam.ac.uk','mac.com','rsmart.com','mtu.edu','gmail.com','northwestern.edu','weber.edu'],
+['2005-12',5,4,4,3,2,2,2,2,2,2]
+];
